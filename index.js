6bc01e7e38a21aae2c2851d811b3f9dcba9b3264
Calvein <calvein@gmail.com>
2016-07-09 21:20:42 +1000

    Add env vars

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ const languages = [
 const controller = Botkit.slackbot({
     json_file_store: './db_slackbutton_slash_command/',
 }).configureSlackApp({
-    clientId: config.clientId,
-    clientSecret: config.clientSecret,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     scopes: ['commands'],
 })
 
-controller.setupWebserver(config.port, (err, webserver) => {
+controller.setupWebserver(process.env.PORT, (err, webserver) => {
     controller.createWebhookEndpoints(controller.webserver)
     controller.createOauthEndpoints(controller.webserver, (err, req, res) => {
         if (err) {
@@ -31,18 +31,18 @@ controller.setupWebserver(config.port, (err, webserver) => {
 
 
 controller.on('slash_command', (slashCommand, msg) => {
-    // Make sure the token matches
-    if (msg.token !== config.verificationToken) return
+    if (msg.command !== '/pok') {
+        return slashCommand.replyPublic(msg, `I'm afraid I don't know how to ${msg.command} yet.`)
+    }
 
-    // Prepare regex
+    // Make sure the token matches!
+    if (msg.token !== process.env.VERIFICATION_TOKEN) return
+
     const search = new RegExp(msg.text, 'i')
-
-    // Create text to send
     const text = pokemons
         .filter((list) => list.some((pokemon) => search.test(pokemon)))
         .map((d) => d.map((name, i) => `*${languages[i]}:* ${name}`).join('\n'))
         .join('\n---\n')
 
-    // Send message
     slashCommand.replyPublic(msg, text)
 })
